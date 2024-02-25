ef4695e13abf9741edc9513ce4167999f98f7244
aziz <abikbest05@gmail.com>
2024-02-26 03:21:38 +0600

    success_url updated

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView, DetailView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Recipe
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
@@ -23,7 +24,7 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
     form_class = RecipeForm
     template_name = 'recipes/recipe_form.html'
-    success_url = reverse_lazy('recipe_list')
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
