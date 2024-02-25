e59e77c3842f1858ad81ff001e7d33bd539ebcdf
aziz <abikbest05@gmail.com>
2024-02-25 23:53:14 +0600

    users/urls connected in project urls

--- myrecipesite/urls.py
+++ myrecipesite/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from myrecipesite.settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include("users.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
