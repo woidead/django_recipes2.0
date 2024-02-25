ca888e2c8909e20065b93b416cdd832efe012107
aziz <abikbest05@gmail.com>
2024-02-26 03:21:18 +0600

    forms updated

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,23 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Recipe
 
 class RecipeForm(ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class':'form-control', 'placeholder':'Введите название рецепта'
+        })
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class':'form-control', 'placeholder':'Введите описание рецепта'
+        })
+    )
+    instructions = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class':'form-control', 'placeholder':'Введите инструкцию к приготовлению'
+        })
+    )
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'instructions']
