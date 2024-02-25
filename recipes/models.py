c8cb35d1c6740671a9f0438ff5dec92eb0a79ea7
aziz <abikbest05@gmail.com>
2024-02-26 01:05:26 +0600

    created Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    instructions = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
