ab8d728ed5e4b3948e9dd7bdbb62622e51af7b6c
aziz <abikbest05@gmail.com>
2024-02-26 02:58:13 +0600

    created path for views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from recipes.views import RecipeListView
+from recipes.views import RecipeListView, RecipeDetailView, RecipeCreateView
 
 urlpatterns = [
     path('', RecipeListView.as_view(), name='home'),
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/add/', RecipeCreateView.as_view(), name='recipe_add'),
 ]
