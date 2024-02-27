e1dd735b5164788ab8abb988a6f5d26fe8cf10fb
woidead <abikbest05@gmail.com>
2024-02-27 15:28:43 +0600

    created model Comment

--- recipes/models.py
+++ recipes/models.py
@@ -9,3 +9,17 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:20]  # Возвращает первые 20 символов комментария
+
+
+# python manage.py makemigrations
+# python manage.py migrate
