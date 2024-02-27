73f827ac78620a11443750c04ccd563a89be6618
woidead <abikbest05@gmail.com>
2024-02-27 15:28:26 +0600

    added commentaries for paths

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,10 @@ from recipes.views import RecipeListView, RecipeDetailView, RecipeCreateView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
+    # Главная страница и список рецептов
     path('', RecipeListView.as_view(), name='home'),
+    # Индивидуальная страница для каждого рецепта
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    # Добавление нового рецепта
     path('recipe/add/', login_required(RecipeCreateView.as_view()), name='recipe_add'),
 ]
