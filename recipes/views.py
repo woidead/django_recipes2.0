c08e6507f48f461ef1774b8b8a5ae59b0c1e272b
woidead <abikbest05@gmail.com>
2024-02-27 15:38:59 +0600

    added methods in RecipeDetailView

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Recipe
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-from .forms import RecipeForm
+from .forms import RecipeForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -19,6 +19,19 @@ class RecipeDetailView(DetailView):
     context_object_name = 'recipe'
     template_name = 'recipes/recipe_detail.html'
 
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.recipe = self.get_object()
+            comment.author = request.user
+            comment.save()
+            return redirect('recipe_detail', pk=comment.recipe.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
 
 class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
